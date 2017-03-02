3ddbdcf9b74db8322de088a82fee3ee087dfbc32
yujoengin <yujoengin123@gmail.com>
2017-03-02 16:49:31 +0900

    edited location address

--- edit.php
+++ edit.php
@@ -12,22 +12,22 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <!-- 위 3개의 메타 태그는 *반드시* head 태그의 처음에 와야합니다; 어떤 다른 콘텐츠들은 반드시 이 태그들 *다음에* 와야 합니다 -->
-	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="/page/style.css">
 	<!-- 부트스트랩 -->
-	<link href="http://localhost/bootstrap-3.3.4-dist/css/bootstrap.min.css" rel="stylesheet">
+	<link href="/bootstrap-3.3.4-dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
 	<div class="container">
 		<header class="jumbotron text-center" > <!-- It doesn't affect to this document. Only means header part -->
-			<img src="https://s3.ap-northeast-2.amazonaws.com/opentutorials-user-file/course/94.png">
-			<h1><a href="http://localhost/page/index.php">Javascript</a></h1>
+			<img src="https://scontent-icn1-1.xx.fbcdn.net/v/t1.0-9/734639_385303771568291_681294276_n.jpg?oh=d796a8d14e0ab56e42d3324ba9648939&oe=59411B6C" class="img-circle" id="logo">
+			<h1 id="headerText"><a href="/page/index.php">YUJOENGIN</a></h1>
 		</header>
 		<div class="row">
 			<nav class="col-md-2"> <!-- It doesn't affect to this document. Only means nav part -->
 				<ol class="nav nav-pills nav-stacked">
 					<?php
 						while ($row = mysqli_fetch_assoc($result)) {
-						 	echo '<li><a href="http://localhost/page/index.php?id='.$row['id'].'">'.$row['title'].'</a></li>'."\n";
+						 	echo '<li><a href="/page/index.php?id='.$row['id'].'">'.$row['title'].'</a></li>'."\n";
 				 		}
 					?>
 				</ol>
@@ -37,9 +37,9 @@
 					<div id="control">
 						<input type="button" name="" value="white" id="white_btn" class="btn btn-default"/>
 						<input type="button" name="" value="black" id="black_btn" class="btn btn-default"/>
-						<a href="http://localhost/page/write.php" class="btn btn-primary">쓰기</a>
-						<a href="http://localhost/page/delete.php" class="btn btn-primary">지우기</a>
-						<a href="http://localhost/page/edit.php" class="btn btn-primary">수정</a>
+						<a href="/page/write.php" class="btn btn-primary">쓰기</a>
+						<a href="/page/delete.php" class="btn btn-primary">지우기</a>
+						<a href="/page/edit.php" class="btn btn-primary">수정</a>
 					</div>
 				</p>
 				<hr>
@@ -68,7 +68,7 @@
 				</p>
 			</nav>
 		</div>
-		<script src="http://localhost/page/script.js"></script>
+		<script src="/page/script.js"></script>
 	</div>
 </body>
 </html>
--- index.php
+++ index.php
@@ -12,22 +12,22 @@ $result = mysqli_query($conn,'SELECT * FROM topic');
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <!-- 위 3개의 메타 태그는 *반드시* head 태그의 처음에 와야합니다; 어떤 다른 콘텐츠들은 반드시 이 태그들 *다음에* 와야 합니다 -->
-	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="/page/style.css">
 	<!-- 부트스트랩 -->
-	<link href="http://localhost/bootstrap-3.3.4-dist/css/bootstrap.min.css" rel="stylesheet">
+	<link href="/bootstrap-3.3.4-dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
 	<div class="container">
-		<header class="jumbotron text-center" > <!-- It doesn't affect to this document. Only means header part -->
-			<img src="https://s3.ap-northeast-2.amazonaws.com/opentutorials-user-file/course/94.png" class="img-circle">
-			<h1><a href="http://localhost/page/index.php">Javascript</a></h1>
+		<header class="jumbotron text-center"> <!-- It doesn't affect to this document. Only means header part -->
+			<img src="https://scontent-icn1-1.xx.fbcdn.net/v/t1.0-9/734639_385303771568291_681294276_n.jpg?oh=d796a8d14e0ab56e42d3324ba9648939&oe=59411B6C" class="img-circle" id="logo">
+			<h1 id="headerText"><a href="/page/index.php">YUJOENGIN</a></h1>
 		</header>
 		<div class="row">
 			<nav class="col-md-2"> <!-- It doesn't affect to this document. Only means nav part -->
 				<ol class="nav nav-pills nav-stacked">
 					<?php
 						while ($row = mysqli_fetch_assoc($result)) {
-						 	echo '<li><a href="http://localhost/page/index.php?id='.$row['id'].'">'.htmlspecialchars($row['title']).'</a></li>'."\n";
+						 	echo '<li><a href="/page/index.php?id='.$row['id'].'">'.htmlspecialchars($row['title']).'</a></li>'."\n";
 				 		}
 					?>
 				</ol>
@@ -37,9 +37,9 @@ $result = mysqli_query($conn,'SELECT * FROM topic');
 					<div id="control">
 						<input type="button" name="" value="white" id="white_btn" class="btn btn-default"/>
 						<input type="button" name="" value="black" id="black_btn" class="btn btn-default"/>
-						<a href="http://localhost/page/write.php" class="btn btn-primary">쓰기</a>
-						<a href="http://localhost/page/delete.php" class="btn btn-primary">지우기</a>
-						<a href="http://localhost/page/edit.php" class="btn btn-primary">수정</a>
+						<a href="/page/write.php" class="btn btn-primary">쓰기</a>
+						<a href="/page/delete.php" class="btn btn-primary">지우기</a>
+						<a href="/page/edit.php" class="btn btn-primary">수정</a>
 					</div>
 				</p>
 				<hr>
@@ -64,11 +64,11 @@ $result = mysqli_query($conn,'SELECT * FROM topic');
 				</p>
 			</nav>
 		</div>
-		<script src="http://localhost/page/script.js"></script>
+		<script src="/page/script.js"></script>
 		<!-- jQuery (부트스트랩의 자바스크립트 플러그인을 위해 필요합니다) -->
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 		    <!-- 모든 컴파일된 플러그인을 포함합니다 (아래), 원하지 않는다면 필요한 각각의 파일을 포함하세요 -->
-		<script src="http://localhost/bootstrap-3.3.4-dist/js/bootstrap.min.js"></script>
+		<script src="/bootstrap-3.3.4-dist/js/bootstrap.min.js"></script>
 	</div>
 
 </body>
